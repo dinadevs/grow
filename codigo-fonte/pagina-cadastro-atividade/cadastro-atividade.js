abd9e98948f894e37b959127868ed65cd64e9eeb
Naiara-Andrade <naiaraandraderodrigues@gmail.com>
2025-05-18 22:33:53 -0300

    feat: elabora código do cadastro-atividade.js

--- codigo-fonte/pagina-cadastro-atividade/cadastro-atividade.js
+++ codigo-fonte/pagina-cadastro-atividade/cadastro-atividade.js
@@ -2,3 +2,18 @@ carregaComponente('../global/componentes/cabecalho/cabecalho.html', 'cabecalho-g
 carregaComponente('../global/componentes/barra-status/barra-status.html', 'barra-status', '../global/componentes/barra-status/barra-status.js');
 
 
+
+function salvarAtividade(e) {
+  e.preventDefault(); 
+
+  const atividades = JSON.parse(localStorage.getItem("Atividades")) || {}; 
+
+  const jogadorLogado = localStorage.getItem("jogadorLogado");
+
+  if (!jogadorLogado) return alert("Nenhum jogador logado!");
+
+  if (!atividades[jogadorLogado]) {
+    atividades[jogadorLogado] = []; 
+  } 
+
+  
